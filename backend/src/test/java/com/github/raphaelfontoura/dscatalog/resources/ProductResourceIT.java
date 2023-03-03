4ee88bfae80f9e4a5b163e24391a690c9b1f616b
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-03-03 15:49:30 -0300

    Add transactional to integrated tests

--- backend/src/test/java/com/github/raphaelfontoura/dscatalog/resources/ProductResourceIT.java
+++ backend/src/test/java/com/github/raphaelfontoura/dscatalog/resources/ProductResourceIT.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class ProductResourceIT {
 
     @Autowired
